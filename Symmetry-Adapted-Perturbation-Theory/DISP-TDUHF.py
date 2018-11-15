408dc905eead2e6c105be00862f1f677710305aa
Filip <filip.brzek@gmail.com>
2018-11-15 10:27:43 -0600

    Initial implementation od second order dispersion from TDUHF

---
+++ Symmetry-Adapted-Perturbation-Theory/DISP-TDUHF.py
@@ -0,0 +1,339 @@
+# FIXME: Add proper citations
+"""
+A Psi4 input script to compute second order dispersion out of TDUHF response. As a note this is, by far,
+not the most efficiently algorithm, but certainly the most verbose.
+References:
+- TDHF equations and algorithms taken from [Amos:1985:2186] and [Helgaker:2000]
+- Gauss-Legendre integration from [Amos:1985:2186] and [Jiemchooroj:2006:124306]
+"""
+
+__authors__ = "Daniel G. A. Smith"
+__credits__ = ["Daniel G. A. Smith", "Filip Brzek"]
+
+__copyright__ = "(c) 2014-2018, The Psi4NumPy Developers"
+__license__ = "BSD-3-Clause"
+__date__ = "2018-11-14"
+
+import time
+import numpy as np
+import scipy.linalg as la
+np.set_printoptions(precision=6, linewidth=200, threshold=2000, suppress=True)
+import psi4
+from helper_RPA import *
+
+
+# Set memory & output file
+psi4.set_memory('2048 MB')
+psi4.core.set_output_file('output.dat', False)
+psi4.core.set_num_threads(2)
+
+# Set molecule to dimer (xyz coordinates, default angstroms)
+dimer = psi4.geometry("""
+Ar 0.000 0.000 -3.470
+--
+H  0.000 0.000  0.000
+F  0.000 0.000  0.920
+""")
+
+#dimer = psi4.geometry("""
+##0 2
+##Li  0.000 0.000 -1.48169297
+#He  0.000 0.000 -1.48169297
+#--
+##0 2
+##Li  0.000 0.000  1.48169297
+#He  0.000 0.000  1.48169297
+#""")
+
+#basis = "sto-3g"
+#basis = "3-21G"
+#basis = "6-31G"
+basis = "aug-cc-pvtz"
+
+psi4.set_options({"basis" : basis,
+                  "scf_type": "df",
+                  #"scf_type": "direct",
+                  "reference": "uhf",
+                  #"save_jk": True,
+                  "guess": "sad",
+                  "e_convergence": 1e-13,
+                  "d_convergence": 1e-13})
+
+# 'stolen' from helper_SAPT.py (Psi4NumPy)
+dimer.reset_point_group('c1')
+dimer.fix_orientation(True)
+dimer.fix_com(True)
+dimer.update_geometry()
+
+# sanity on monomers
+nfrags = dimer.nfragments()
+if nfrags != 2:
+    psi4.core.clean()
+    raise Exception("Found {} fragments, must be 2.".format(nfrags))
+
+# mon geometries
+monomerA = dimer.extract_subsets(1, 2)
+monomerA.set_name("MonomerA")
+
+monomerB = dimer.extract_subsets(2, 1)
+monomerB.set_name("MonomerB")
+
+# FIXME: Add handling if nocc for UHF is zero, e.g. H
+t = time.time()
+# monomerA wfn optimization
+scf_e_A, wfnA = psi4.energy('SCF', return_wfn=True, molecule=monomerA)
+
+# monomerB wfn optimization
+scf_e_B, wfnB = psi4.energy('SCF', return_wfn=True, molecule=monomerB)
+
+print('SCF took                %5.3f seconds' % ( time.time() - t))
+
+print("")
+print("[DEBUG]: E_A: {} E_B: {}".format(scf_e_A, scf_e_B)) # agrees with reference implementation
+
+# AO-MO coeffs in DCSB
+# monA
+Co_A_a = wfnA.Ca_subset("AO", "OCC")
+Cv_A_a = wfnA.Ca_subset("AO", "VIR")
+eps_A_a = np.asarray(wfnA.epsilon_a())
+
+Co_A_b = wfnA.Cb_subset("AO", "OCC")
+Cv_A_b = wfnA.Cb_subset("AO", "VIR")
+eps_A_b = np.asarray(wfnA.epsilon_b())
+
+# monB
+Co_B_a = wfnB.Ca_subset("AO", "OCC")
+Cv_B_a = wfnB.Ca_subset("AO", "VIR")
+eps_B_a = np.asarray(wfnB.epsilon_a())
+
+Co_B_b = wfnB.Cb_subset("AO", "OCC")
+Cv_B_b = wfnB.Cb_subset("AO", "VIR")
+eps_B_b = np.asarray(wfnB.epsilon_b())
+
+# sanity check
+assert(wfnA.nmo() == wfnB.nmo())
+nbf = wfnA.nmo()
+
+# occupied orbitals
+nocc_A_a = wfnA.nalpha()
+nocc_A_b = wfnA.nbeta()
+
+nocc_B_a = wfnB.nalpha()
+nocc_B_b = wfnB.nbeta()
+
+# virtual orbitals
+nvir_A_a = nbf - nocc_A_a
+nvir_A_b = nbf - nocc_A_b
+
+nvir_B_a = nbf - nocc_B_a
+nvir_B_b = nbf - nocc_B_b
+
+# occ*virt block sizes
+nov_A_a = nocc_A_a * nvir_A_a # number of excitations (occ -> virt). Size of (ar) block
+nov_A_b = nocc_A_b * nvir_A_b # number of excitations (occ -> virt). Size of (ar) block
+
+nov_B_a = nocc_B_a * nvir_B_a
+nov_B_b = nocc_B_b * nvir_B_b
+
+print("Mon: A")
+print("Nocc  (alpha):  {:4d}\tNocc  (beta):  {:4d}  (o)".format(nocc_A_a,
+                                                                nocc_A_b))
+print("Nvir  (alpha):  {:4d}\tNvir  (beta):  {:4d}  (v)".format(nvir_A_a,
+                                                                nvir_A_b))
+print("Nrot  (alpha):  {:4d}\tNrot  (beta):  {:4d}  (ov)".format(nov_A_a,
+                                                                 nov_A_b))
+print("")
+print("Mon: B")
+print("Nocc  (alpha):  {:4d}\tNocc  (beta):  {:4d}  (o)".format(nocc_B_a,
+                                                                nocc_B_b))
+print("Nvir  (alpha):  {:4d}\tNvir  (beta):  {:4d}  (v)".format(nvir_B_a,
+                                                                nvir_B_b))
+print("Nrot  (alpha):  {:4d}\tNrot  (beta):  {:4d}  (ov)".format(nov_B_a,
+                                                                 nov_B_b))
+print("")
+
+# orb energies
+# monA
+eps_v_A_a = eps_A_a[nocc_A_a:]
+eps_v_A_b = eps_A_b[nocc_A_b:]
+
+eps_o_A_a = eps_A_a[:nocc_A_a]
+eps_o_A_b = eps_A_b[:nocc_A_b]
+
+# monB
+eps_v_B_a = eps_B_a[nocc_B_a:]
+eps_v_B_b = eps_B_b[nocc_B_b:]
+
+eps_o_B_a = eps_B_a[:nocc_B_a]
+eps_o_B_b = eps_B_b[:nocc_B_b]
+
+# Integral generation from Psi4's MintsHelper
+t = time.time()
+mints = psi4.core.MintsHelper(wfnA.basisset())
+S = np.asarray(mints.ao_overlap())
+I = mints.ao_eri()
+# ERI transformation
+# NOTE: SAPT notation in the comments, CC(?) notation in the var name (TD-HF.py) taken from Psi4NumPy
+# NOTE: SAPT a(b) -> occupied of monA(monB); r(s) -> virtual of monA(monB)
+
+########
+# monA #
+########
+# v_oovv
+# (i, a): alpha; (j, b): alpha
+v_ijab_A_aa = np.asarray(mints.mo_transform(I, Co_A_a, Co_A_a, Cv_A_a, Cv_A_a))
+# (i, a): beta; (j, b): beta
+v_ijab_A_bb = np.asarray(mints.mo_transform(I, Co_A_b, Co_A_b, Cv_A_b, Cv_A_b))
+# (i, a): alpha; (j, b): beta
+#v_ijab_A_ab = np.asarray(mints.mo_transform(I, Co_A_a, Co_A_a, Cv_A_b, Cv_A_b))
+# (i, a): beta; (j, b): alpha
+#v_ijab_A_ba = np.asarray(mints.mo_transform(I, Co_A_b, Co_A_b, Cv_A_b, Cv_A_b))
+print("[DEBUG] monA: ERI(ijab) (alpha, alpha) shape: {}".format(v_ijab_A_aa.shape))
+print("[DEBUG] monA: ERI(ijab) (beta,  beta)  shape: {}".format(v_ijab_A_bb.shape))
+#print("[DEBUG] monA: ERI(ijab) (alpha, beta)  shape: {}".format(v_ijab_A_ab.shape))
+#print("[DEBUG] monA: ERI(ijab) (beta,  alpha) shape: {}".format(v_ijab_A_ba.shape))
+# v_ovov
+# (i, a): alpha; (j, b): alpha
+v_iajb_A_aa = np.asarray(mints.mo_transform(I, Co_A_a, Cv_A_a, Co_A_a, Cv_A_a))
+v_iajb_A_bb = np.asarray(mints.mo_transform(I, Co_A_b, Cv_A_b, Co_A_b, Cv_A_b))
+v_iajb_A_ab = np.asarray(mints.mo_transform(I, Co_A_a, Cv_A_a, Co_A_b, Cv_A_b))
+v_iajb_A_ba = np.asarray(mints.mo_transform(I, Co_A_b, Cv_A_b, Co_A_a, Cv_A_a))
+print("[DEBUG] monA: ERI(iajb) (alpha, alpha) shape: {}".format(v_iajb_A_aa.shape))
+print("[DEBUG] monA: ERI(iajb) (beta,  beta)  shape: {}".format(v_iajb_A_bb.shape))
+print("[DEBUG] monA: ERI(iajb) (alpha, beta)  shape: {}".format(v_iajb_A_ab.shape))
+print("[DEBUG] monA: ERI(iajb) (beta,  alpha) shape: {}".format(v_iajb_A_ba.shape))
+
+########
+# monB #
+########
+#
+# v_oovv
+#
+# (i, a): alpha; (j, b): alpha
+v_ijab_B_aa = np.asarray(mints.mo_transform(I, Co_B_a, Co_B_a, Cv_B_a, Cv_B_a))
+# (i, a): beta; (j, b): beta
+v_ijab_B_bb = np.asarray(mints.mo_transform(I, Co_B_b, Co_B_b, Cv_B_b, Cv_B_b))
+# (i, a): alpha; (j, b): beta
+#v_ijab_B_ab = np.asarray(mints.mo_transform(I, Co_B_a, Co_B_a, Cv_B_b, Cv_B_b))
+# (i, a): beta; (j, b): alpha
+#v_ijab_B_ba = np.asarray(mints.mo_transform(I, Co_B_b, Co_B_b, Cv_B_b, Cv_B_b))
+
+# debug
+print("[DEBUG] monB: ERI(ijab) (alpha, alpha) shape: {}".format(v_ijab_B_aa.shape))
+print("[DEBUG] monB: ERI(ijab) (beta,  beta)  shape: {}".format(v_ijab_B_bb.shape))
+#print("[DEBUG] monB: ERI(ijab) (alpha, beta)  shape: {}".format(v_ijab_B_ab.shape))
+#print("[DEBUG] monB: ERI(ijab) (beta,  alpha) shape: {}".format(v_ijab_B_ba.shape)
+#
+# v_ovov
+#
+# (i, a): alpha; (j, b): alpha
+v_iajb_B_aa = np.asarray(mints.mo_transform(I, Co_B_a, Cv_B_a, Co_B_a, Cv_B_a))
+# (i, a): beta; (j, b): beta
+v_iajb_B_bb = np.asarray(mints.mo_transform(I, Co_B_b, Cv_B_b, Co_B_b, Cv_B_b))
+# (i, a): alpha; (j, b): beta
+v_iajb_B_ab = np.asarray(mints.mo_transform(I, Co_B_a, Cv_B_a, Co_B_b, Cv_B_b))
+# (i, a): beta; (j, b): alpha
+v_iajb_B_ba = np.asarray(mints.mo_transform(I, Co_B_b, Cv_B_b, Co_B_a, Cv_B_a))
+
+# debug
+print("[DEBUG] monB: ERI(iajb) (alpha, alpha) shape: {}".format(v_iajb_B_aa.shape))
+print("[DEBUG] monB: ERI(iajb) (beta,  beta)  shape: {}".format(v_iajb_B_bb.shape))
+print("[DEBUG] monB: ERI(iajb) (alpha, beta)  shape: {}".format(v_iajb_B_ab.shape))
+print("[DEBUG] monB: ERI(iajb) (beta,  alpha) shape: {}".format(v_iajb_B_ba.shape))
+
+#########
+# dimer #
+#########
+#   AABB
+# v_ovov
+# (i, a): alpha; (j, b): alpha
+v_AB_aa = np.asarray(mints.mo_transform(I, Co_A_a, Cv_A_a, Co_B_a, Cv_B_a))
+# (i, a): beta; (j, b): beta
+v_AB_bb = np.asarray(mints.mo_transform(I, Co_A_b, Cv_A_b, Co_B_b, Cv_B_b))
+# (i, a): alpha; (j, b): beta
+v_AB_ab = np.asarray(mints.mo_transform(I, Co_A_a, Cv_A_a, Co_B_b, Cv_B_b))
+# (i, a): beta; (j, b): alpha
+v_AB_ba = np.asarray(mints.mo_transform(I, Co_A_b, Cv_A_b, Co_B_a, Cv_B_a))
+
+print("[DEBUG] dimer: ERI(iajb) (alpha, alpha) shape: {}".format(v_AB_aa.shape))
+print("[DEBUG] dimer: ERI(iajb) (beta, beta) shape: {}".format(v_AB_bb.shape))
+print("[DEBUG] dimer: ERI(iajb) (alpha, beta) shape: {}".format(v_AB_ab.shape))
+print("[DEBUG] dimer: ERI(iajb) (beta, alpha) shape: {}".format(v_AB_ba.shape))
+
+Co_A_a = np.asarray(Co_A_a)
+Co_A_b = np.asarray(Co_A_b)
+Cv_A_a = np.asarray(Cv_A_a)
+Cv_A_b = np.asarray(Cv_A_b)
+
+Co_B_a = np.asarray(Co_B_a)
+Co_B_b = np.asarray(Co_B_b)
+Cv_B_a = np.asarray(Cv_B_a)
+Cv_B_b = np.asarray(Cv_B_b)
+print('Integral transform took %5.3f seconds\n' % ( time.time() - t))
+
+print("Building hessians for monomers ...\n")
+print("MonA: ")
+eig_A, Z_A = tduhf_eigen(
+    ((nocc_A_a, nocc_A_b), (nvir_A_a, nvir_A_b),),
+    ((eps_o_A_a, eps_o_A_b), (eps_v_A_a, eps_v_A_b)),
+    ((v_ijab_A_aa, v_ijab_A_bb),
+     (v_iajb_A_aa, v_iajb_A_bb, v_iajb_A_ab, v_iajb_A_ba)),
+     (nov_A_a, nov_A_b))
+
+print("MonB: ")
+eig_B, Z_B = tduhf_eigen(
+    ((nocc_B_a, nocc_B_b), (nvir_B_a, nvir_B_b),),
+    ((eps_o_B_a, eps_o_B_b), (eps_v_B_a, eps_v_B_b)),
+    ((v_ijab_B_aa, v_ijab_B_bb),
+     (v_iajb_B_aa, v_iajb_B_bb, v_iajb_B_ab, v_iajb_B_ba)),
+     (nov_B_a, nov_B_b))
+
+print("Calculating dispersion energy ...")
+# UHF version same as eq. 37-39 (only a->r, b->s excitations)
+# "Dispersion interaction of high-spin open-shell complexes in the random phase approximation"
+# JCP, 119, 10497 (2003), doi: 10.1063/1.1620496
+
+# reshape dimer ERIs
+v_AB_aa.shape = (nov_A_a, nov_B_a)
+v_AB_bb.shape = (nov_A_b, nov_B_b)
+v_AB_ab.shape = (nov_A_a, nov_B_b)
+v_AB_ba.shape = (nov_A_b, nov_B_a)
+
+v1 = np.hstack((v_AB_aa, v_AB_ab))
+v2 = np.hstack((v_AB_ba, v_AB_bb))
+v_AB = np.vstack((v1, v2))
+
+t = time.time()
+
+# Hapka&Zuchowski SAPT(DFT) 2013
+E = np.dot(Z_A.real.T, v_AB)
+D = np.dot(E, Z_B.real)
+
+# helper for denominator reshaping
+shape = lambda dim: (-1,) + tuple([1]*(dim -1))
+
+omegas_pq = 1/((eig_A.real**0.5).reshape(shape(2)) + (eig_B.real**0.5).reshape(shape(1)))
+
+t_pq = np.einsum('pq,pq->pq', D, omegas_pq)
+E_2disp = -1*np.einsum('pq,pq', D, t_pq)
+
+print('TDUHF DISP TOOK: %5.3f seconds\n' % ( time.time() - t))
+# from Hapka&Zuchowski molpro code
+# ArHF
+ref = {
+        'sto-3g': -0.00073146046940817,
+        '3-21G' : -0.02331354,
+        '6-31G' : -0.02793545,
+        'aug-cc-pvdz' : -0.18362697
+      }
+# He2
+#ref = {
+#        'sto-3g': -0.42271566e-8,
+#        '6-31G' : -0.12789154e-3,
+#        'aug-cc-pvdz' : -0.04751493
+#      }
+
+e_ref = ref.get(basis)
+
+print("E^2_disp: {0} should be {1}".format(E_2disp*1000, e_ref))
+print("{}".format(E_2disp*1000/e_ref))
---
+++ Symmetry-Adapted-Perturbation-Theory/helper_RPA.py
@@ -0,0 +1,204 @@
+# FIXME: Add proper citations
+"""
+Helper functions for the TDUHF response in SAPT directory.
+
+References:
+- Equations and algorithms from [Szalewicz:2005:43], [Jeziorski:1994:1887],
+[Szalewicz:2012:254], and [Hohenstein:2012:304]
+"""
+
+__authors__   = "Daniel G. A. Smith"
+__credits__   = ["Daniel G. A. Smith", "Filip Brzek"]
+
+__copyright__ = "(c) 2014-2018, The Psi4NumPy Developers"
+__license__   = "BSD-3-Clause"
+__date__      = "2018-11-14"
+
+import numpy as np
+import time
+import scipy.linalg as la
+
+__all__ = ['tduhf_eigen']
+
+def tduhf_eigen(occ, eps, v_eri, nov):
+    '''
+    Implements generalized eigenvalue problem for TDUHF
+    (A+B)(A-B)Z = w^{2}Z
+    '''
+    nocc, nvir = occ
+    # alpha & beta
+    nocc_a, nocc_b = nocc
+    nvir_a, nvir_b = nvir
+
+    eps_o, eps_v = eps
+    # alpha & beta
+    eps_o_a, eps_o_b = eps_o
+    eps_v_a, eps_v_b = eps_v
+
+    # different spin blocks
+    v_ijab, v_iajb = v_eri
+    v_ijab_aa, v_ijab_bb = v_ijab
+    v_iajb_aa, v_iajb_bb, v_iajb_ab, v_iajb_ba = v_iajb
+
+    nov_a, nov_b = nov
+
+    t = time.time()
+    # Building A and B blocks
+    # 4 cases:
+    # X_aa: (alpha, alpha),
+    # X_ab: (alpha, beta),
+    # X_ba: (beta,  alpha),
+    # X_bb: (beta,  beta)
+
+
+    # FIXME: DFT needs special treatment for v_xc kernel part
+    #################
+    # alpha & alpha #
+    #################
+    A_aa  = np.einsum('ab,ij->iajb', np.diag(eps_v_a), np.diag(np.ones(nocc_a)))
+    A_aa -= np.einsum('ij,ab->iajb', np.diag(eps_o_a), np.diag(np.ones(nvir_a)))
+    A_aa += v_iajb_aa
+    A_aa -= v_ijab_aa.swapaxes(1, 2)
+    print('[DEBUG] A_aa raw shape {}'.format(A_aa.shape))
+
+    B_aa  = v_iajb_aa.copy()
+    B_aa -= v_iajb_aa.swapaxes(1, 3)
+
+    print('[DEBUG] B_aa raw shape {}'.format(B_aa.shape))
+
+    A_aa.shape = (nov_a, nov_a)
+    B_aa.shape = (nov_a, nov_a)
+    print('[DEBUG] A/B_aa block shape {}'.format(A_aa.shape))
+
+    ###############
+    # beta & beta #
+    ###############
+    print("")
+    A_bb  = np.einsum('ab,ij->iajb', np.diag(eps_v_b), np.diag(np.ones(nocc_b)))
+    A_bb -= np.einsum('ij,ab->iajb', np.diag(eps_o_b), np.diag(np.ones(nvir_b)))
+    A_bb += v_iajb_bb
+    A_bb -= v_ijab_bb.swapaxes(1, 2)
+    print('[DEBUG] A_bb raw shape {}'.format(A_bb.shape))
+
+    B_bb  = v_iajb_bb.copy()
+    B_bb -= v_iajb_bb.swapaxes(1, 3)
+
+    print('[DEBUG] B_bb raw shape {}'.format(B_bb.shape))
+
+    # Reshape and jam it together
+    A_bb.shape = (nov_b, nov_b)
+    B_bb.shape = (nov_b, nov_b)
+    print('[DEBUG] A/B_bb block shape {}'.format(A_bb.shape))
+
+    # No Exchange part for opposite spin electrons
+    # B_(ai)(bj) = A_(ai)(bj) = (ai|bj)
+    ################
+    # alpha & beta #
+    ################
+    print("")
+    A_ab = v_iajb_ab.copy()
+    print('[DEBUG] A_ab raw shape {}'.format(A_ab.shape))
+
+    B_ab  = v_iajb_ab.copy()
+    print('[DEBUG] B_ab raw shape {}'.format(B_ab.shape))
+
+    A_ab.shape = (nov_a, nov_b)
+    B_ab.shape = (nov_a, nov_b)
+    print('[DEBUG] A/B_ab block shape {}'.format(A_ab.shape))
+
+    ################
+    # beta & alpha #
+    ################
+    print("")
+    A_ba = v_iajb_ba.copy()
+    print('[DEBUG] A_ba raw shape {}'.format(A_ba.shape))
+
+    B_ba  = v_iajb_ab.copy()
+    print('[DEBUG] B_ba raw shape {}'.format(B_ba.shape))
+
+    A_ba.shape = (nov_b, nov_a)
+    B_ba.shape = (nov_b, nov_a)
+    print('[DEBUG] A/B_ba block shape {}'.format(A_ba.shape))
+
+
+    A1 = np.hstack((A_aa, A_ab))
+    A2 = np.hstack((A_ba, A_bb))
+    A = np.vstack((A1, A2))
+    print('[DEBUG] A shape {}'.format(A.shape))
+
+    B1 = np.hstack((B_aa, B_ab))
+    B2 = np.hstack((B_ba, B_bb))
+    B = np.vstack((B1, B2))
+    print('[DEBUG] B shape {}'.format(B.shape))
+
+    H1 = A + B
+    H2 = A - B
+
+    #print("H1: ")
+    #print(H1)
+    #print("H2: ")
+    #print(H2)
+    ##########################
+    # TAKE CARE WE GOING HAM #
+    # FOR He2 in 6-31G       #
+    ##########################
+    ##dimer = psi4.geometry("""
+    ##He  0.000 0.000 -1.48169297
+    ##--
+    ##He  0.000 0.000  1.48169297
+    ##""")
+
+    #H1 = [[1.167223, -0.008676, -0.000107, 0.000340, 0.011439, 0.000070],
+    #      [-0.008676, 1.684294, -0.004767, 0.011439, 0.455045, 0.003777],
+    #      [-0.000107, -0.004767, 4.954139, 0.000070, 0.003777, 0.000046],
+    #      [0.000340, 0.011439, 0.000070, 1.167233, -0.008676, -0.000107],
+    #      [0.011439, 0.455045, 0.003777, -0.008676, 1.684294, -0.004767],
+    #      [0.000070, 0.003777, 0.000046, -0.000107, -0.004767, 4.954139],]
+    #H2 = [[1.167223, -0.008676, -0.000107, 0.0, 0.0, 0.0],
+    #      [-0.008676, 1.684294, -0.004767, 0.0, 0.0, 0.0],
+    #      [-0.000107, -0.004767, 4.954139, 0.0, 0.0, 0.0],
+    #      [0.0, 0.0, 0.0, 1.167233, -0.008676, -0.000107],
+    #      [0.0, 0.0, 0.0, -0.008676, 1.684294, -0.004767],
+    #      [0.0, 0.0, 0.0, -0.000107, -0.004767, 4.954139],]
+
+    # NOTE: This eigenproblem formulation is not Hermitian, thus
+    # NOTE: less numerically stable.
+    # TDUHF eigenproblem for omega and Z
+    # (A-B)(A+B)Z = w**2 Z
+    # Z = X + Y
+    #LHS_Hess = np.dot(H2, H1)
+
+    # NOTE: Hermitian Eigenproblem
+    # Hirata, Gordon (1999)
+    # 10.1016/S0009-2614(99)00137-2
+    # (A-B)^(1/2)(A+B)(A-B)^(1/2) Z = w**2 Z
+    # Z = (A-B)^(-1/2) (X+Y)
+    H2_sqrt = la.sqrtm(H2)
+    LHS_Hess = np.dot(H2_sqrt, H1)
+    LHS_Hess = np.dot(LHS_Hess, H2_sqrt)
+
+    # eigh matters
+    eig, vect = la.eigh(LHS_Hess)
+
+    # sorting doesn't change anything
+    idx = eig.argsort()[::-1]
+    eig = eig[idx]
+    vect = vect[:, idx]
+
+    print("[DEBUG]: Eigs shape: {}".format(eig.shape))
+    print("[DEBUG]: Vect shape: {}".format(vect.shape))
+
+    for v in range(nov_a + nov_b):
+        w = eig[v].real**0.5
+        w_sqrt = w**0.5
+        z = vect[:,v].real
+        # fixing Z normalization in hermitian eigenproblem
+        z *= w**(-0.5)
+        if abs(eig[v].imag) > 1e-13:
+            print("[Warning] Large imaginary part! Im(omega) = {:3e}".format(eig[v].imag))
+
+    print('Generalized eingenvalue took:  %5.3f seconds\n' % ( time.time() - t))
+
+    # fixing Z normalization in hermitian eigenproblem
+    vect = np.dot(H2_sqrt, vect)
+    return eig, vect
